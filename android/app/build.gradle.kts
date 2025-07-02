8966647627b91e3806346bc55c0e409411bfc7f9
Tito <joaogtito@gmail.com>
2025-07-02 09:19:16 -0300

    correção

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,6 +29,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        resValue("string", "google_maps_key", project.findProperty("MAPS_API_KEY") as String? ?: "")
     }
 
     buildTypes {
